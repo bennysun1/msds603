4a146f450f6071da7dffb60d3329e5a434ed0812
Ben Sunshine <bsunshine25@gmail.com>
2025-04-21 18:31:52 -0700

    Add MLFlow orchestration with training and scoring flows

--- src/scoringflow.py
+++ src/scoringflow.py
@@ -15,19 +15,15 @@ class ModelScoringFlow(FlowSpec):
     @step
     def start(self):
         """
-        Starting point: Load the trained model from MLFlow.
-        Gets the latest version of the registered model.
+        Starting point: Load the trained model from the latest training run.
         """
         # Set up MLFlow tracking
         mlflow.set_tracking_uri('http://127.0.0.1:5000')
+        mlflow.set_experiment('metaflow-wine-experiment')
         
-        # Load the model from MLFlow Model Registry
-        model_name = "wine-classifier"
-        model_version = 1  # You might want to make this a parameter
-        
-        self.model = mlflow.sklearn.load_model(
-            model_uri=f"models:/{model_name}/{model_version}"
-        )
+        # Get the latest training run
+        run = Flow('ModelTrainingFlow').latest_run
+        self.model = run['end'].task.data.model
         
         print("Input vector:", self.vector)
         self.next(self.end)
@@ -39,7 +35,8 @@ class ModelScoringFlow(FlowSpec):
         Uses the loaded model to predict the class of the input vector.
         """
         print('Using Model:', self.model)
-        print('Predicted class:', self.model.predict([self.vector])[0])
+        prediction = self.model.predict([self.vector])[0]
+        print('Predicted class:', prediction)
 
 
 if __name__=='__main__':
